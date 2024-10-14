aa03069d93fb739e667e5c61c98b8528702fd162
Jhondrek <jhondrek116@gmail.com>
2024-10-14 16:47:02 -0600

    display menu and working on displaying bill

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import {menuArray} from "./data.js"
 
-console.log(menuArray)
+const menuItemsContainer = document.getElementById("menu-items-container")
+
+renderMenu()
+
+function renderMenu(){
+    let menuHtml = ""
+    menuArray.forEach((menuItem)=>{
+        menuHtml += `
+            <div class="dish-container">   
+                <p class="emoji">${menuItem.emoji}</p>
+                <div class="dish-information-container">
+                    <h2>${menuItem.name}</h2>
+                    <p class="ingredients">${menuItem.ingredients}</p>
+                    <p class="price">$${menuItem.price}</p>
+                </div>
+                <button class="fa-solid fa-plus addButton"></button>
+            </div>` 
+    })
+
+    menuItemsContainer.innerHTML = menuHtml
+    
+
+}
